10eb199681c72398581062d15474842168d718ab
hrithik-hg <68283247+hrithik-hg@users.noreply.github.com>
2022-07-20 12:51:47 +0530

    Update news_summarization.py

--- News_Summary/news_summarization.py
+++ News_Summary/news_summarization.py
@@ -1,25 +1,22 @@
-from sklearn.model_selection import train_test_split
 import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
 
 """# Preprocessing The Data"""
 
 df = pd.read_csv('news_summary.csv', encoding = 'Latin-1')
-df.head()
 
 df=df[["text","ctext"]]
-df.head()
 
 """# T5Data with Training Data Column Name:- sorce_text & target_text"""
 
 df.columns = ["target_text","source_text"]
 df = df.dropna()
 df=df[["source_text","target_text"]]
-df.head()
 
 """# T5 Data with Summarize prefix"""
 
 df["source_text"] = "summarize: " + df["source_text"]
-df.head()
 
 """# Splitting the dataset into Training and Test dataframe"""
 
@@ -42,11 +39,12 @@ model.train(train_df = train_df[: 5000],
             target_max_token_len = 128,
             batch_size = 8, max_epochs = 5, use_gpu = True)
 
-! (cd outputs; ls)
+print(os.listdir('outputs'))
 
 """# Inference"""
 
-model.load_model("t5", "outputs/simplet5-epoch-4-train-loss-1.0731-val-loss-1.4669", use_gpu = True)
+path_ = input("Enter the least error Epoch path: ")
+model.load_model("t5", "outputs/"+path_, use_gpu = True)
 
 """# News Summary Prediction"""
 
@@ -60,4 +58,4 @@ The Trump administration has previously fixed May 1 as a possible date to reopen
 
 preprocess_text = text.strip().replace("\n","")
 prepared_Text = "summarize: " + preprocess_text
-model.predict(prepared_Text)
+print(model.predict(prepared_Text))
