dcf5800fbd9796bf160b7a9a121591d0bec79cc7
hrithik-hg <68283247+hrithik-hg@users.noreply.github.com>
2022-07-22 12:40:31 +0530

    Update news_summarization.py

--- News_Summary/news_summarization.py
+++ News_Summary/news_summarization.py
@@ -23,40 +23,34 @@ df["source_text"] = "summarize: " + df["source_text"]
 train_df, test_df = train_test_split(df, test_size=0.3)
 train_df.shape, test_df.shape
 
-"""# Using SimpleT5 for Model Training- Instantiate, Download Pre-trained Model
-
-# Model Training
-"""
+"""# Using SimpleT5 for Model Training- Instantiate, Download Pre-trained Model"""
 
 from simplet5 import SimpleT5
 
 model = SimpleT5()
 model.from_pretrained(model_type = "t5", model_name='t5-base')
 
+"""# Model Training"""
+
 model.train(train_df = train_df[: 5000],
             eval_df = test_df[: 100],
-            source_max_token_len = 512,
-            target_max_token_len = 128,
-            outputdir = "outputs",
-            batch_size = 8, max_epochs = 5, use_gpu = True)
+            source_max_token_len = 128,
+            target_max_token_len = 50,
+            batch_size = 8, max_epochs = 1, use_gpu = False, outputdir = "outputs")
 
 print(os.listdir('outputs'))
 
 """# Inference"""
 
 path_ = input("Enter the least error Epoch path: ")
-model.load_model("t5", "outputs/"+path_, use_gpu = True)
+model.load_model("t5", "outputs/"+path_, use_gpu = False)
 
-"""# News Summary Prediction"""
 
-text ="""
-The US has "passed the peak" on new coronavirus cases, President Donald Trump said and predicted that some states would reopen this month.
-The US has over 637,000 confirmed Covid-19 cases and over 30,826 deaths, the highest for any country in the world.
-At the daily White House coronavirus briefing on Wednesday, Trump said new guidelines to reopen the country would be announced on Thursday after he speaks to governors.
-"We'll be the comeback kids, all of us," he said. "We want to get our country back."
-The Trump administration has previously fixed May 1 as a possible date to reopen the world's largest economy, but the president said some states may be able to return to normalcy earlier than that.
-"""
+"""# Headlines Prediction"""
+
+text = input("Enter the text:- ")
 
 preprocess_text = text.strip().replace("\n","")
-prepared_Text = "summarize: " + preprocess_text
-print(model.predict(prepared_Text))
+news_summary = "summarize: " + preprocess_text
+print("The Summary for the News is:- ", end="")
+print(model.predict(news_summary))
