f81e8a877c9651b2c7078f76f9525de692ed40c3
hrithik-hg <68283247+hrithik-hg@users.noreply.github.com>
2022-07-22 12:36:55 +0530

    Update headlines.py

--- Headlines/headlines.py
+++ Headlines/headlines.py
@@ -36,27 +36,21 @@ model.train(train_df = train_df[: 5000],
             eval_df = test_df[: 100],
             source_max_token_len = 128,
             target_max_token_len = 50,
-            outputdir = "outputs",
-            batch_size = 8, max_epochs = 5, use_gpu = True)
+            batch_size = 8, max_epochs = 1, use_gpu = False, outputdir = "outputs")
 
 print(os.listdir('outputs'))
 
 """# Inference"""
 
 path_ = input("Enter the least error Epoch path: ")
-model.load_model("t5", "outputs/"+path_, use_gpu = True)
+model.load_model("t5", "outputs/"+path_, use_gpu = False)
 
 
 """# Headlines Prediction"""
 
-text ="""
-The US has "passed the peak" on new coronavirus cases, President Donald Trump said and predicted that some states would reopen this month.
-The US has over 637,000 confirmed Covid-19 cases and over 30,826 deaths, the highest for any country in the world.
-At the daily White House coronavirus briefing on Wednesday, Trump said new guidelines to reopen the country would be announced on Thursday after he speaks to governors.
-"We'll be the comeback kids, all of us," he said. "We want to get our country back."
-The Trump administration has previously fixed May 1 as a possible date to reopen the world's largest economy, but the president said some states may be able to return to normalcy earlier than that.
-"""
+text = input("Enter the text:- ")
 
 preprocess_text = text.strip().replace("\n","")
-prepared_Text = "summarize: " + preprocess_text
-print(model.predict(prepared_Text))
+headline = "summarize: " + preprocess_text
+print("The headline for the text is:- ", end="")
+print(model.predict(headline))
